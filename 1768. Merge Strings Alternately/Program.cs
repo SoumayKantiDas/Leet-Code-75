8e54211be57afcdb1183f923663f12c2c402f0e1
Nondita Mondal <72224262+SoumayKantiDas@users.noreply.github.com>
2024-05-07 21:22:52 +0600

    180148

--- 1768. Merge Strings Alternately/Program.cs
+++ 1768. Merge Strings Alternately/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please insert the word1");
+        
+        
+        string word1 = Console.ReadLine();
+        Console.WriteLine("Please insert the word2");
+        string word2 = Console.ReadLine();
+        string mixed = MergeAlternately(word1, word2);
+        Console.WriteLine(mixed);
+        
+    }
+    public static string MergeAlternately(string word1, string word2)
+    {
+        int len1 = word1.Length;
+        int len2 = word2.Length;
+
+        char[] output = new char[len1 + len2];
+        int i = 0;  int j = 0; int k = 0;
+        while (i < len1 && j < len2 )
+        {
+            output[k++] = word1[i++];
+            output[k++] = word2[j++];
+        }
+        if ( len1 > len2 ) 
+        {
+            while(i < len1)
+            {
+                output[k++] = word1[i++];
+            }
+
+        }
+        else
+        {
+            while (j < len2)
+            {
+                output[k++] = word2[j++];
+            }
+        }
+        return new string(output);
+
+    }
+}
