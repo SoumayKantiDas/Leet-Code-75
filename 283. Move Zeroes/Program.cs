272df811fcbbb8ffe42c2cb8a28a64c7167b264c
Nondita Mondal <72224262+SoumayKantiDas@users.noreply.github.com>
2024-07-18 00:48:17 +0600

    180148

---
+++ 283. Move Zeroes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        List<int> nonZeroElements = new List<int>();
+
+        
+        foreach (int num in nums)
+        {
+            if (num != 0)
+            {
+                nonZeroElements.Add(num);
+            }
+        }
+
+       
+        for (int i = 0; i < nonZeroElements.Count; i++)
+        {
+            nums[i] = nonZeroElements[i];
+        }
+
+      
+        for (int i = nonZeroElements.Count; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] nums = { 0, 1, 0, 3, 12 };
+
+        Solution solution = new Solution();
+        solution.MoveZeroes(nums);
+
+        Console.WriteLine("The array after moving zeroes:");
+        Console.WriteLine(string.Join(",", nums));
+    }
+}
---
+++ SubSequence Matching/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string s = "Asc";
+        string t = "Astc";
+        bool isSubsequence = solution.IsSubsequence(s, t);
+        Console.WriteLine($"Is '{s}' a subsequence of '{t}'? {isSubsequence}");
+    }
+
+    public class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int sPointer = 0;
+            int tPointer = 0;
+            int sLength = s.Length;
+            int tLength = t.Length;
+
+            while (sPointer < sLength && tPointer < tLength)
+            {
+                if (s[sPointer] == t[tPointer])
+                {
+                    sPointer++;
+                }
+                tPointer++;
+            }
+
+            return sPointer == sLength;
+        }
+    }
+}
