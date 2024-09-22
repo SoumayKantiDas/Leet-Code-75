ffabe9df3b9e93a6a4193c51a1cb00e3b1b7a3d7
Nondita Mondal <72224262+SoumayKantiDas@users.noreply.github.com>
2024-09-22 16:48:50 +0600

    180148

---
+++ Find the Highest Altitude/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        int[] arrr = [-5, 1, 5, 0, -7];
+
+        int d = solution.LargestAltitude(arrr);
+
+        Console.WriteLine(d);
+    }
+    public class Solution
+    {
+        public int LargestAltitude(int[] gain)
+        {
+            int max = 0;
+            int current = 0;    
+
+            foreach (int i in gain)
+            {
+                current += i;
+
+                if (current > max)
+                {
+                    max = current;
+                }
+            }
+            return max;
+        }
+    }
+}
